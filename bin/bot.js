88ad1915d90c7a11ae9c5ac64f69a6548ba4340a
Jonathon Kelly <jonathon@insidersbyte.com>
2016-10-19 17:03:33 +0100

    :sparkles: use es6 class

--- bin/bot.js
+++ bin/bot.js
@@ -5,10 +5,6 @@ const token = process.env.BOT_API_KEY;
 const dbPath = process.env.BOT_DB_PATH;
 const name = process.env.BOT_NAME;
 
-const norrisbot = new NorrisBot({
-    token,
-    dbPath,
-    name,
-});
+const norrisbot = new NorrisBot({ token, dbPath, name });
 
 norrisbot.run();
