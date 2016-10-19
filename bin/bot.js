5a499db40ebb74b97ef98f0d7d96330f80e46fd2
Jonathon Kelly <jonathon@insidersbyte.com>
2016-10-19 16:26:29 +0100

    :tada: initial commit

---
+++ bin/bot.js
@@ -0,0 +1,14 @@
+require('dotenv').config({ silent: true });
+const NorrisBot = require('../lib/norrisbot');
+
+const token = process.env.BOT_API_KEY;
+const dbPath = process.env.BOT_DB_PATH;
+const name = process.env.BOT_NAME;
+
+const norrisbot = new NorrisBot({
+    token,
+    dbPath,
+    name,
+});
+
+norrisbot.run();
